a99af357a59c0649c86aa947bf75d5161fddb4dc
Luke Dusautoy <ldusa012@uottawa.ca>
2023-06-07 13:50:38 -0400

    modal finished

--- jquery.js
+++ jquery.js
@@ -1,4 +1,12 @@
-$('#appointment-info').on('submit', function(ev) {
-    $('#myModal').modal('show'); 
+$('#appointment-info').on('submit', function (ev) {
+    document.getElementById("nameModal").innerHTML = document.getElementById("name").value;
+    document.getElementById("serviceModal").innerHTML = document.getElementById("service").value;
+    document.getElementById("phoneModal").innerHTML = document.getElementById("phone").value;
+    document.getElementById("expertModal").innerHTML = document.getElementById("expert").value;
+    document.getElementById("patientsModal").innerHTML = document.getElementById("patient").value;
+    document.getElementById("emailModal").innerHTML = document.getElementById("email").value;
+    document.getElementById("dateTimeModal").innerHTML = document.getElementById("datetime").value;
+
+    $('#myModal').modal('show');
     ev.preventDefault();
 });
